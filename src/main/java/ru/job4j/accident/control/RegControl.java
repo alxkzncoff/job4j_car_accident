537e833670e1b3d24e290b24434662a97c106b45
Aleksandr <alx.kzncoff@gmail.com>
2022-08-09 10:57:29 +0300

    change variable names

--- src/main/java/ru/job4j/accident/control/RegControl.java
+++ src/main/java/ru/job4j/accident/control/RegControl.java
@@ -12,23 +12,23 @@ import ru.job4j.accident.repository.UserRepository;
 @Controller
 public class RegControl {
     private final PasswordEncoder encoder;
-    private final UserRepository users;
-    private final AuthorityRepository authorities;
+    private final UserRepository userRepository;
+    private final AuthorityRepository authorityRepository;
 
-    public RegControl(PasswordEncoder encoder, UserRepository users, AuthorityRepository authorities) {
+    public RegControl(PasswordEncoder encoder, UserRepository userRepository, AuthorityRepository authorityRepository) {
         this.encoder = encoder;
-        this.users = users;
-        this.authorities = authorities;
+        this.userRepository = userRepository;
+        this.authorityRepository = authorityRepository;
     }
 
     @PostMapping("/reg")
     public String regSave(@ModelAttribute User user) {
         String fail = "true";
-        if (users.findByName(user.getUsername()).isEmpty()) {
+        if (userRepository.findByName(user.getUsername()).isEmpty()) {
             user.setEnabled(true);
             user.setPassword(encoder.encode(user.getPassword()));
-            user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-            users.save(user);
+            user.setAuthority(authorityRepository.findByAuthority("ROLE_USER"));
+            userRepository.save(user);
             fail = "false";
         }
         return String.format("redirect:/login?fail=%s", fail);
